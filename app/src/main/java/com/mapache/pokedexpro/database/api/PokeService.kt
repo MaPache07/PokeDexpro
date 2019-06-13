4e68d790c8af8a135f8301688c79c9e8d03ed971
MaPache07 <00170517@uca.edu.sv>
2019-06-12 21:45:15 -0600

    Se agregaron dependencias y se crearon otras clases

--- app/src/main/java/com/mapache/pokedexpro/database/api/PokeService.kt
+++ app/src/main/java/com/mapache/pokedexpro/database/api/PokeService.kt
@@ -1,9 +1,11 @@
 package com.mapache.pokedexpro.database.api
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.mapache.pokedexpro.database.entities.PokeApi
 import kotlinx.coroutines.Deferred
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 const val POKE_DB = "https://pokeapi.co/api/v2/pokemon/"
@@ -14,8 +16,9 @@ interface PokeService {
     fun getPoke() : Deferred<Response<PokeApi>>
 
     companion object{
-        /*fun getInstance() : PokeService{
-            return Retrofit.Builder().baseUrl(POKE_DB).addConverterFactory()
-        }*/
+        fun getInstance() : PokeService {
+            return Retrofit.Builder().baseUrl(POKE_DB).addConverterFactory(MoshiConverterFactory.create())
+                .addCallAdapterFactory(CoroutineCallAdapterFactory()).build().create(PokeService::class.java)
+        }
     }
 }
